d481618cb0a11789cdf4ab58c7eea1b06a2cb71f
manish <manish.sethi@gmail.com>
2022-08-27 20:10:48 -0400

    Enhanced sig_verifier_mgmt and added basic test

--- pipeline/sig_verifier_mgmt.go
+++ pipeline/sig_verifier_mgmt.go
@@ -13,65 +13,60 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type sigVerifierMgrConfig struct {
-	SigVerifierServers       []string
-	ValidsBatchSize          int
-	ValidsBatchTimeoutMillis int
+type SigVerifierMgrConfig struct {
+	SigVerifierServers []string
+	BatchCutConfig     *BatchCutConfig
+}
+
+type BatchCutConfig struct {
+	BatchSize     int
+	TimeoutMillis int
 }
 
 type sigVerifierMgr struct {
-	config              *sigVerifierMgrConfig
+	config              *SigVerifierMgrConfig
 	verifiers           []*sigVerifier
 	nextVerifier        int
 	verificationResults *sigVerificationResults
-}
-
-type sigVerifier struct {
-	stream sigverification.Verifier_StartStreamClient
-	sendCh chan *token.Block
+	stopSignalCh        chan struct{}
+	stopWg              sync.WaitGroup
 }
 
 type sigVerificationResults struct {
-	validsBatchSize          int
-	ValidsBatchTimeoutMillis int
-	batchAvailableSignal     chan struct{}
-	validsCh                 chan []txSeqNum
-	invalidsCh               chan []txSeqNum
-
-	mu     sync.Mutex
-	valids []txSeqNum
+	batchCutConfig         *BatchCutConfig
+	batchCutTimeoutResetCh chan struct{}
+	validsCh               chan []txSeqNum
+	invalidsCh             chan []txSeqNum
+	mu                     sync.Mutex
+	valids                 []txSeqNum
 }
 
-func newSigVerificationMgr(c *sigVerifierMgrConfig) (*sigVerifierMgr, error) {
+func newSigVerificationMgr(c *SigVerifierMgrConfig) (*sigVerifierMgr, error) {
 	verificationResults := &sigVerificationResults{
-		validsBatchSize:      c.ValidsBatchSize,
-		batchAvailableSignal: make(chan struct{}, defaultChannelBufferSize),
-		validsCh:             make(chan []txSeqNum, defaultChannelBufferSize),
-		invalidsCh:           make(chan []txSeqNum, defaultChannelBufferSize),
-		valids:               []txSeqNum{},
+		batchCutConfig:         c.BatchCutConfig,
+		batchCutTimeoutResetCh: make(chan struct{}),
+		validsCh:               make(chan []txSeqNum, defaultChannelBufferSize),
+		invalidsCh:             make(chan []txSeqNum, defaultChannelBufferSize),
+		valids:                 []txSeqNum{},
 	}
-	verificationResults.startTimeoutBasedBatchCutterRoutine()
 
 	verifiers := []*sigVerifier{}
-	for _, m := range c.SigVerifierServers {
-		s, err := initSigVerifierStream(m)
+	for _, h := range c.SigVerifierServers {
+		v, err := newSigVerifier(h, verificationResults)
 		if err != nil {
 			return nil, err
 		}
-		v := &sigVerifier{
-			stream: s,
-			sendCh: make(chan *token.Block, defaultChannelBufferSize),
-		}
-		v.startRequestSenderRoutine()
-		v.startResponseRecieverRoutine(verificationResults)
 		verifiers = append(verifiers, v)
 	}
 
-	return &sigVerifierMgr{
+	m := &sigVerifierMgr{
 		config:              c,
 		verifiers:           verifiers,
 		verificationResults: verificationResults,
-	}, nil
+		stopSignalCh:        make(chan struct{}),
+	}
+	m.startTimeoutBasedBatchCutterRoutine()
+	return m, nil
 }
 
 func (m *sigVerifierMgr) processBlockAsync(b *token.Block) {
@@ -84,107 +79,199 @@ func (m *sigVerifierMgr) processBlockAsync(b *token.Block) {
 	v.sendCh <- b
 }
 
-func (v *sigVerifier) startRequestSenderRoutine() {
-	go func() {
-		for {
-			b := <-v.sendCh
-			reqs := make([]*sigverification.Request, len(b.Txs))
-			for i, tx := range b.Txs {
-				reqs[i] = &sigverification.Request{
-					BlockNum: b.Number,
-					TxNum:    uint64(i),
-					Tx:       tx,
-				}
-			}
-
-			err := v.stream.Send(&sigverification.RequestBatch{Requests: reqs})
-
-			if err != nil {
-				panic("Error while sending sig verification request batch on stream")
-			}
-		}
-	}()
-}
-
-func (v *sigVerifier) startResponseRecieverRoutine(r *sigVerificationResults) {
-	for {
-		responseBatch, err := v.stream.Recv()
-		if err != nil {
-			panic("Error while reaading sig verification response batch on stream")
-		}
-
-		responses := responseBatch.Responses
-		valids := []txSeqNum{}
-		invalids := []txSeqNum{}
-
-		for _, r := range responses {
-			n := txSeqNum{
-				blkNum: r.BlockNum,
-				txNum:  r.TxNum,
-			}
-			if r.IsValid {
-				valids = append(valids, n)
-			} else {
-				invalids = append(invalids, n)
-			}
-		}
-
-		r.invalidsCh <- invalids
-
-		func() {
-			r.mu.Lock()
-			defer r.mu.Unlock()
-			r.valids = append(r.valids, valids...)
-			// cut batch based on size
-			if len(r.valids) >= r.validsBatchSize {
-				b := r.valids[:r.validsBatchSize]
-				r.valids = r.valids[r.validsBatchSize:]
-				r.validsCh <- b
-			}
-		}()
+func (r *sigVerificationResults) cutBatchWhileHoldingLock() {
+	batchSize := r.batchCutConfig.BatchSize
+	size := len(r.valids)
+	if size == 0 {
+		return
 	}
+
+	if size > batchSize {
+		size = batchSize
+	}
+
+	b := r.valids[:size]
+	r.valids = r.valids[size:]
+	r.validsCh <- b
 }
 
-func (r *sigVerificationResults) startTimeoutBasedBatchCutterRoutine() {
+func (m *sigVerifierMgr) startTimeoutBasedBatchCutterRoutine() {
+	timeoutMillis := m.config.BatchCutConfig.TimeoutMillis
+	d := time.Duration(timeoutMillis * int(time.Millisecond))
+	r := m.verificationResults
 	go func() {
 		for {
 			select {
-			case <-r.batchAvailableSignal:
-			case <-time.After(time.Duration(r.ValidsBatchTimeoutMillis * int(time.Millisecond))):
+			case <-m.stopSignalCh:
+				m.stopWg.Done()
+				return
+			case <-r.batchCutTimeoutResetCh:
+			case <-time.After(d):
 				func() {
 					r.mu.Lock()
 					defer r.mu.Unlock()
-					size := len(r.valids)
-
-					if size == 0 {
+					if len(r.batchCutTimeoutResetCh) > 0 {
+						// the batch already cut based on size before we aquired lock here
 						return
 					}
-
-					if size > r.validsBatchSize {
-						size = r.validsBatchSize
-					}
-
-					b := r.valids[:size]
-					r.valids = r.valids[size:]
-					r.validsCh <- b
+					r.cutBatchWhileHoldingLock()
 				}()
 			}
 		}
 	}()
 }
 
-func initSigVerifierStream(machine string) (sigverification.Verifier_StartStreamClient, error) {
+func (m *sigVerifierMgr) stopTimeoutBasedBatchCutterRoutine() {
+	m.stopWg.Add(1)
+	m.stopSignalCh <- struct{}{}
+	m.stopWg.Wait()
+}
+
+func (m *sigVerifierMgr) stop() {
+	for _, v := range m.verifiers {
+		v.stop()
+	}
+	m.stopTimeoutBasedBatchCutterRoutine()
+
+	r := m.verificationResults
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	for len(r.valids) != 0 {
+		r.cutBatchWhileHoldingLock()
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+type sigVerifier struct {
+	stream              sigverification.Verifier_StartStreamClient
+	streamContext       context.Context
+	streamContextCancel func()
+	sendCh              chan *token.Block
+
+	stopSignalCh    chan struct{}
+	stoppedSignalCh chan struct{}
+	stopWG          sync.WaitGroup
+}
+
+func newSigVerifier(host string, resultsDest *sigVerificationResults) (*sigVerifier, error) {
 	conn, err := grpc.Dial(
-		fmt.Sprintf("%s:%d", machine, config.GRPC_PORT),
+		fmt.Sprintf("%s:%d", host, config.GRPC_PORT),
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 	if err != nil {
 		return nil, err
 	}
 	client := sigverification.NewVerifierClient(conn)
-	stream, err := client.StartStream(context.Background())
+
+	cancelableContext, cancel := context.WithCancel(context.Background())
+	stream, err := client.StartStream(cancelableContext)
 	if err != nil {
 		return nil, err
 	}
-	return stream, err
+
+	v := &sigVerifier{
+		stream:              stream,
+		streamContext:       cancelableContext,
+		streamContextCancel: cancel,
+		sendCh:              make(chan *token.Block, defaultChannelBufferSize),
+		stopSignalCh:        make(chan struct{}, 2),
+		stoppedSignalCh:     make(chan struct{}, 2),
+	}
+
+	v.startRequestSenderRoutine()
+	v.startResponseRecieverRoutine(resultsDest)
+	return v, nil
+}
+
+func (v *sigVerifier) startRequestSenderRoutine() {
+	go func() {
+		for {
+			select {
+			case <-v.stopSignalCh:
+				err := v.stream.CloseSend()
+				if err != nil {
+					panic(fmt.Sprintf("Error while closing the stream for sending: %s", err))
+				}
+				v.stopWG.Done()
+				return
+			case b := <-v.sendCh:
+				reqs := make([]*sigverification.Request, len(b.Txs))
+				for i, tx := range b.Txs {
+					reqs[i] = &sigverification.Request{
+						BlockNum: b.Number,
+						TxNum:    uint64(i),
+						Tx:       tx,
+					}
+				}
+
+				err := v.stream.Send(&sigverification.RequestBatch{Requests: reqs})
+				if err != nil {
+					panic(fmt.Sprintf("Error while sending sig verification request batch on stream: %s", err))
+				}
+			}
+		}
+	}()
+}
+
+func (v *sigVerifier) startResponseRecieverRoutine(r *sigVerificationResults) {
+	go func() {
+		for {
+			responseBatch, err := v.stream.Recv()
+			if v.streamContext.Err() == context.Canceled {
+				v.stopWG.Done()
+				return
+			}
+
+			if err != nil {
+				panic(fmt.Sprintf("Error while reaading sig verification response batch on stream: %s", err))
+			}
+
+			responses := responseBatch.Responses
+			valids := []txSeqNum{}
+			invalids := []txSeqNum{}
+
+			for _, res := range responses {
+				n := txSeqNum{
+					blkNum: res.BlockNum,
+					txNum:  res.TxNum,
+				}
+				if res.IsValid {
+					valids = append(valids, n)
+				} else {
+					invalids = append(invalids, n)
+				}
+			}
+
+			r.invalidsCh <- invalids
+
+			func() {
+				batchSize := r.batchCutConfig.BatchSize
+				r.mu.Lock()
+				defer r.mu.Unlock()
+				r.valids = append(r.valids, valids...)
+				if len(r.valids) >= batchSize {
+					r.cutBatchWhileHoldingLock()
+					r.batchCutTimeoutResetCh <- struct{}{}
+				}
+			}()
+		}
+	}()
+}
+
+func (v *sigVerifier) stopRequestSenderRoutine() {
+	v.stopWG.Add(1)
+	v.stopSignalCh <- struct{}{}
+	v.stopWG.Wait()
+}
+
+func (v *sigVerifier) stopResponseRecieverRoutine() {
+	v.stopWG.Add(1)
+	v.streamContextCancel()
+	v.stopWG.Wait()
+}
+
+func (v *sigVerifier) stop() {
+	v.stopRequestSenderRoutine()
+	v.stopResponseRecieverRoutine()
 }
---
+++ pipeline/sig_verifier_mgmt_test.go
@@ -0,0 +1,124 @@
+package pipeline
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.ibm.com/distributed-trust-research/scalable-committer/config"
+	"github.ibm.com/distributed-trust-research/scalable-committer/sigverification"
+	"github.ibm.com/distributed-trust-research/scalable-committer/token"
+	"google.golang.org/grpc"
+)
+
+func TestSigVerifiersMgr(t *testing.T) {
+	s := &testSigVerifierServer{t: t}
+	s.start()
+	defer s.stop()
+
+	m, err := newSigVerificationMgr(
+		&SigVerifierMgrConfig{
+			SigVerifierServers: []string{"localhost"},
+			BatchCutConfig: &BatchCutConfig{
+				BatchSize:     2,
+				TimeoutMillis: 20000,
+			},
+		},
+	)
+	assert.NoError(t, err)
+	defer m.stop()
+
+	g := &testBlockGenerator{}
+	b := g.generateNextBlock(2)
+	m.processBlockAsync(b)
+
+	txSeqNums := <-m.verificationResults.validsCh
+	require.Equal(t, 2, len(txSeqNums))
+	require.Equal(t, txSeqNum{blkNum: 0, txNum: 0}, txSeqNums[0])
+	require.Equal(t, txSeqNum{blkNum: 0, txNum: 1}, txSeqNums[1])
+}
+
+type testSigVerifierServer struct {
+	t          *testing.T
+	grpcServer *grpc.Server
+
+	sigverification.UnimplementedVerifierServer
+}
+
+func (s *testSigVerifierServer) start() {
+	address := fmt.Sprintf("localhost:%d", config.GRPC_PORT)
+	s.grpcServer = grpc.NewServer()
+	go func() {
+
+		sigverification.RegisterVerifierServer(s.grpcServer, s)
+		lis, err := net.Listen("tcp", address)
+		require.NoError(s.t, err)
+		require.NoError(s.t, s.grpcServer.Serve(lis))
+	}()
+
+	for i := 0; i < 10; i++ {
+		conn, err := net.DialTimeout("tcp", address, 1*time.Second)
+		if err != nil {
+			if i == 9 {
+				s.t.Fatal(err)
+			}
+			time.Sleep(10 * time.Millisecond)
+			continue
+		}
+		require.NoError(s.t, conn.Close())
+		break
+	}
+}
+
+func (s *testSigVerifierServer) stop() {
+	s.grpcServer.GracefulStop()
+}
+
+func (s *testSigVerifierServer) StartStream(stream sigverification.Verifier_StartStreamServer) error {
+	for {
+		requestBatch, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+
+		responseBatch := &sigverification.ResponseBatch{}
+
+		for _, request := range requestBatch.Requests {
+			responseBatch.Responses = append(responseBatch.Responses, &sigverification.Response{
+				BlockNum: request.BlockNum,
+				TxNum:    request.TxNum,
+				IsValid:  true,
+			})
+		}
+		require.NoError(s.t, stream.Send(responseBatch))
+	}
+}
+
+func (s *testSigVerifierServer) SetVerificationKey(context context.Context, key *sigverification.Key) (*sigverification.Empty, error) {
+	return nil, nil
+}
+
+type testBlockGenerator struct {
+	nextBlockNum uint64
+}
+
+func (g *testBlockGenerator) generateNextBlock(numTx int) *token.Block {
+	b := &token.Block{
+		Number: g.nextBlockNum,
+	}
+	for i := 0; i < numTx; i++ {
+		t := &token.Tx{
+			SerialNumbers: [][]byte{[]byte("Junk Serial Number")},
+		}
+		b.Txs = append(b.Txs, t)
+	}
+	return b
+}
