972e512aa7c56922654ff26a51cb0475fcd5bb30
Julian Castrence <juliancastrence@ibm.com>
2022-09-21 12:54:33 -0400

    Deployment Scripts Improvements 2

--- scripts/copy_and_run.sh
+++ scripts/copy_and_run.sh
@@ -11,11 +11,26 @@ done
 
 rootDir="$(pwd)"
 
-CONFIG_PATH="${rootDir}/ansible/ansible.cfg"
-PLAYBOOK_PATH="${rootDir}/ansible/playbooks/10-copy-and-run.yaml"
+ANSIBLE_CONFIG_PATH="${rootDir}/ansible/ansible.cfg"
+PLAYBOOKS_PATH="${rootDir}/ansible/playbooks"
 COORDINATOR_PATH="bin/coordinator"
+SERVICES_CONFIG_PATH="${rootDir}/config"
+GENERAL_CONFIG="config.yaml"
+SHARDS_CONFIG="config-shardsservice.yaml"
+SIGVER_CONFIG="config-sigverification.yaml"
 
-export ANSIBLE_CONFIG=${CONFIG_PATH}
-ansible-playbook ${PLAYBOOK_PATH} --extra-vars "arg=$1"
+export ANSIBLE_CONFIG=${ANSIBLE_CONFIG_PATH}
+ansible-playbook "${PLAYBOOKS_PATH}/0-setup-dir.yaml"
+if [ -f "${SERVICES_CONFIG_PATH}/${GENERAL_CONFIG}" ]; then
+    ansible-playbook "${PLAYBOOKS_PATH}/10-copy-general-config.yaml" --extra-vars "filename=${GENERAL_CONFIG}"
+fi
+if [ -f "${SERVICES_CONFIG_PATH}/${SHARDS_CONFIG}" ]; then
+    ansible-playbook "${PLAYBOOKS_PATH}/20-copy-service-config.yaml" --extra-vars "servicename=shards filename=${SHARDS_CONFIG}"
+fi
+if [ -f "${SERVICES_CONFIG_PATH}/${SIGVER_CONFIG}" ]; then
+    ansible-playbook "${PLAYBOOKS_PATH}/20-copy-service-config.yaml" --extra-vars "servicename=sigverification filename=${SIGVER_CONFIG}"
+fi
+ansible-playbook "${PLAYBOOKS_PATH}/30-copy-custom-config.yaml"
+ansible-playbook "${PLAYBOOKS_PATH}/40-copy-and-run-service.yaml" --extra-vars "arg=$1"
 
 ./${COORDINATOR_PATH}
