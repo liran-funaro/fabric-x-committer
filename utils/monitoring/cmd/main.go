4d180bbf14c34e7d3a7b82ec678d9ef43ad4e3f6
Alexandros Filios <alexandros.filios@ibm.com>
2022-09-30 11:57:22 +0200

    Configurable prometheus/grafana main.go
    Added flags to the main.go, so that we can use it as a binary
    
    Signed-off-by: Alexandros Filios <alexandros.filios@ibm.com>

--- utils/monitoring/cmd/main.go
+++ utils/monitoring/cmd/main.go
@@ -1,85 +1,89 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"path/filepath"
 
-	"github.ibm.com/distributed-trust-research/scalable-committer/utils"
 	"github.ibm.com/distributed-trust-research/scalable-committer/utils/logging"
 	"github.ibm.com/distributed-trust-research/scalable-committer/utils/monitoring"
 )
 
-const prometheusDefaultLocalPort = 9090
-const grafanaDefaultLocalPort = 3000
-const grafanaProvisioningDir = "/etc/grafana/provisioning/"
-
-//TODO: Extract to config
-const prometheusMappedPort = 9091
-const grafanaMappedPort = 3001
+const (
+	prometheusDefaultLocalPort = 9090
+	grafanaDefaultLocalPort    = 3000
+	grafanaProvisioningDir     = "/etc/grafana/provisioning/"
+	prometheusInstanceName     = "prometheus-instance"
+)
 
 var logger = logging.New("dockerrunner")
 
 var runOpts = &monitoring.DockerRunOpts{
 	RemoveIfExists: true,
 }
-var prometheusParams = &monitoring.DockerRunParams{
-	Name:     "prometheus-instance",
-	Image:    "prom/prometheus:latest",
-	Hostname: "prometheus",
-	Mounts: map[string]string{
-		configFile("/prometheus.yml"): "/etc/prometheus/prometheus.yml",
-	},
-	PortMappings: map[int]int{
-		prometheusDefaultLocalPort: prometheusMappedPort,
-	},
+
+func prometheusParams(dir string, port int) *monitoring.DockerRunParams {
+	return &monitoring.DockerRunParams{
+		Name:     prometheusInstanceName,
+		Image:    "prom/prometheus:latest",
+		Hostname: "prometheus",
+		Mounts: map[string]string{
+			filepath.Join(dir, "/prometheus.yml"): "/etc/prometheus/prometheus.yml",
+		},
+		PortMappings: map[int]int{
+			prometheusDefaultLocalPort: port,
+		},
+	}
 }
-var grafanaParams = &monitoring.DockerRunParams{
-	Name:     "grafana-instance",
-	Image:    "grafana/grafana:latest",
-	Hostname: "grafana",
-	Envs: map[string]string{
-		"GF_AUTH_PROXY_ENABLED":   "true",
-		"GF_PATHS_PROVISIONING":   grafanaProvisioningDir,
-		"_GF_PROMETHEUS_ENDPOINT": fmt.Sprintf("http://%s:%d", "prometheus-instance", prometheusDefaultLocalPort),
-	},
-	Mounts: map[string]string{
-		configFile("grafana-datasources.yml"):   grafanaProvisioningDir + "datasources/datasource.yml",
-		configFile("grafana-dashboards.yml"):    grafanaProvisioningDir + "dashboards/dashboard.yml",
-		configFile("prometheus-dashboard.json"): grafanaProvisioningDir + "dashboards/prometheus-dashboard.json",
-	},
-	//TODO: Use user-defined network instead of link
-	Links: []string{prometheusParams.Name},
-	PortMappings: map[int]int{
-		grafanaDefaultLocalPort: grafanaMappedPort,
-	},
+func grafanaParams(dir string, port int) *monitoring.DockerRunParams {
+	return &monitoring.DockerRunParams{
+		Name:     "grafana-instance",
+		Image:    "grafana/grafana:latest",
+		Hostname: "grafana",
+		Envs: map[string]string{
+			"GF_AUTH_PROXY_ENABLED":   "true",
+			"GF_PATHS_PROVISIONING":   grafanaProvisioningDir,
+			"_GF_PROMETHEUS_ENDPOINT": fmt.Sprintf("http://%s:%d", "prometheus-instance", prometheusDefaultLocalPort),
+		},
+		Mounts: map[string]string{
+			filepath.Join(dir, "grafana-datasources.yml"):   grafanaProvisioningDir + "datasources/datasource.yml",
+			filepath.Join(dir, "grafana-dashboards.yml"):    grafanaProvisioningDir + "dashboards/dashboard.yml",
+			filepath.Join(dir, "prometheus-dashboard.json"): grafanaProvisioningDir + "dashboards/prometheus-dashboard.json",
+		},
+		//TODO: Use user-defined network instead of link
+		Links: []string{prometheusInstanceName},
+		PortMappings: map[int]int{
+			grafanaDefaultLocalPort: port,
+		},
+	}
 }
 
 func main() {
+	configPath := flag.String("config-dir", ".", "Relative dir path with config files.")
+	prometheusPort := flag.Int("prometheus-port", 9091, "Port Prometheus listens to.")
+	grafanaPort := flag.Int("grafana-port", 3001, "Port Prometheus listens to.")
+	flag.Parse()
+
+	configAbsPath, err := filepath.Abs(*configPath)
+	if err != nil {
+		panic(err)
+	}
 
 	runner, err := monitoring.DockerContainerRunner()
 	if err != nil {
 		panic(err)
 	}
 
-	err = runner.Start(prometheusParams, runOpts)
+	err = runner.Start(prometheusParams(configAbsPath, *prometheusPort), runOpts)
 	if err != nil {
 		logger.Fatal(err)
 	}
 
-	logger.Infof("Prometheus client running on http://localhost:%d", prometheusMappedPort)
+	logger.Infof("Prometheus client running on http://localhost:%d", *prometheusPort)
 
-	err = runner.Start(grafanaParams, runOpts)
+	err = runner.Start(grafanaParams(configAbsPath, *grafanaPort), runOpts)
 	if err != nil {
 		logger.Fatal(err)
 	}
-	logger.Infof("Grafana running on http://localhost:%d (user: admin, pass: admin)", grafanaMappedPort)
-}
-
-func configFile(filename string) string {
-
-	path, err := os.Getwd()
-	utils.Must(err)
-
-	return filepath.Join(path, "config", filename)
+	logger.Infof("Grafana running on http://localhost:%d (user: admin, pass: admin)", *grafanaPort)
 }
