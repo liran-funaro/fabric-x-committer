4c2956a6ea876c6d45f336d717bcbc5cb930268e
Dean Amar <Dean.Amar@ibm.com>
2024-09-10 05:29:21 +0300

    Match Docker service runner script to latest changes in the services command. (#468)
    This commit updates the Docker runner script to run services according
    to the latest changes in the services command.

--- docker/runner/scripts/run-services.sh
+++ docker/runner/scripts/run-services.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-"$BINS_PATH/sigservice" --configs "$CONFIGS_PATH/config-sigservice.yaml" &
+"$BINS_PATH/sigservice" start --configs "$CONFIGS_PATH/config-sigservice.yaml" &
 /home/yugabyte/bin/yugabyted start --advertise_address=0.0.0.0
 
 "$BINS_PATH/vcservice" init --configs "$CONFIGS_PATH/config-vcservice.yaml" --namespaces 0
@@ -17,4 +17,4 @@ while !</dev/tcp/localhost/5001; do sleep 1; done;
 while !</dev/tcp/localhost/5002; do sleep 1; done;
 
 "$BINS_PATH/coordinator_setup" --coordinator :5002 --key-path "$METANS_SIG_VERIFICATION_KEY_PATH" --scheme "$METANS_SIG_SCHEME"
-"$BINS_PATH/sidecar" --configs "$CONFIGS_PATH/config-sidecar.yaml"
+"$BINS_PATH/sidecar" start --configs "$CONFIGS_PATH/config-sidecar.yaml"
